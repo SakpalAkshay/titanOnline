2cedb8d6daacdaa304dda73760431e748095c6f6
Stephanie Becerra <stephanie.becerra.2018@gmail.com>
2023-10-01 21:21:52 -0700

    Updating init.sh

--- populateDB/init.sh
+++ populateDB/init.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 sqlite3 ./titanOnline.db < ./classes.sql
+
+sqlite3 ./titanOnline.db < ./students.sql
+
+sqlite3 ./titanOnline.db < ./instructors.sql
+
+sqlite3 ./titanOnline.db < ./Enrollments.sql
+
+sqlite3 ./titanOnline.db < ./Dropped.sql
+
+sqlite3 ./titanOnline.db < ./Waitlist.sql
