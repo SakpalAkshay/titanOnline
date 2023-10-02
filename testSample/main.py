0c11b5c0da6969a089a0d5e67f8ce0f53b8898ec
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-10-01 20:38:54 -0700

    added drop class function

--- testSample/main.py
+++ testSample/main.py
@@ -53,3 +53,14 @@ def enrollInClass(classId:int, sectionNo:str, db: sqlite3.Connection = Depends(g
     classInfo = db.execute("SELECT * FROM classes WHERE section_number=? AND class_id=?",(sectionNo,classId))
     print(classInfo)
     return {"data":classInfo.fetchall()}
+    #Update current enrollment count for the particular section, add person to enrollment table
+    # also implement if current enrollment is full push the student to waiting list, update the date as well  
+
+@app.put("dropClass/{classId}/{sectionNo}/{studentId}")
+def dropClass(classId:int, sectionNo:str, studentId:int, db: sqlite3.Connection = Depends(get_db) ):
+    #Implement updating the enrollment table i.e drop student from Enrollment table with some student id and classId
+    pass
+    #Imlement updating the dropped tables add student to the drop tables with class Id and section Id from which he has dropped
+    #Update current enrollment count and automatically add someone from waiting list, freeze this enrollment after two weeks,and update the position count in 
+    #waiting list
+
