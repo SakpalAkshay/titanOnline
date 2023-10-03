7483c498117680fe960f4f86a379033604b4d2f1
Snugglefuzz <125339664+Snugglefuzz@users.noreply.github.com>
2023-10-02 23:55:12 -0700

    Update Waitlists.sql
    Changed Date to string format. Date type doesn't exists in SQLite.

--- populateDB/Waitlists.sql
+++ populateDB/Waitlists.sql
@@ -1,4 +1,3 @@
-
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS waitlists;
@@ -7,7 +6,7 @@ CREATE TABLE waitlists(
     student_id INT unsigned NOT NULL, 
     class_id INT unsigned NOT NULL,
     position Int unsigned NOT NULL,
-    date_added DATE NOT NULL,
+    date_added TEXT NOT NULL,
     FOREIGN KEY (student_id) REFERENCES Students(student_id),
     FOREIGN KEY (class_id) REFERENCES classes(class_id),
     PRIMARY KEY (student_id, class_id) 
@@ -16,13 +15,13 @@ CREATE TABLE waitlists(
 INSERT INTO waitlists(student_id, class_id, date_added, position)
 
 VALUES
-    (125, 1, 2023-08-21, 3),
-    (125, 2, 2023-01-18, 2),
-    (125, 3, 2023-01-18, 1),
-    (130, 1, 2024-01-18, 4),
-    (130, 2, 2024-01-25, 5),
-    (130, 3, 2024-02-15, 6),
-    (128, 2, 2024-3-15, 7),
-    (128, 1, 2024-3-15, 8),
-    (124, 1, 2024-4-15, 9);
+    (125, 1, "2023-11-21", 1),
+    (125, 2, "2023-11-22", 2),
+    (125, 3, "2023-11-23", 3),
+    (130, 1, "2023-11-24", 4),
+    (130, 2, "2023-11-25", 5),
+    (130, 3, "2023-12-15", 6),
+    (128, 2, "2023-12-16", 7),
+    (128, 1, "2023-12-18", 8),
+    (124, 1, "2023-12-20", 9);
 COMMIT;
